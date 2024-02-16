337d08be703c6fa56e86155ec6538b0ee4d167e6
Arthur A. <arthur@thewandb.com>
2024-02-16 10:40:56 +0000

    added custom metrics provider

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Import modules
 import { createServer } from "http";
 import { Server } from "socket.io";
 import {
@@ -5,13 +6,14 @@ import {
   type BroadcastMessage,
   type CoreMessage,
 } from "./types/core";
-import { instrument } from "@socket.io/admin-ui";
-import * as console from "console"; // @ts-expect-error Types do not exist for this module
-import ioMetrics from "socket.io-prometheus";
-import { register as promRegister } from "prom-client";
+import * as console from "node:console";
 import express, { json } from "express";
 import cors from "cors";
+import { prometheus, stats } from "./types/metrics";
 
+
+
+// Initialize the server
 const app = express();
 app.use(
   cors({
@@ -21,18 +23,12 @@ app.use(
   }),
 );
 
-app.use(json())
+app.use(json());
 
-const stats: any = {
-  connections: {
-    peak: 0,
-    current: 0,
-  },
-  mem: process.memoryUsage(),
-};
-
+// Create the http server
 const http = createServer(app);
 
+// Initialize the socket.io server
 const io = new Server(http, {
   cors: {
     origin: "*",
@@ -42,38 +38,119 @@ const io = new Server(http, {
   },
 });
 
-instrument(io, {
-  auth: {
-    type: "basic",
-    username: "admin",
-    password: "$2b$10$heqvAkYMez.Va6Et2uXInOnkCT6/uQj1brkrbyG3LpopDklcq7ZOS", // "changeit" encrypted with bcrypt
-  },
-  mode: "development",
-});
+// Initialize the rooms
+const rooms = {
+  youtube: [] as string[],
+  bingo: [] as string[],
+  live: [] as string[],
+};
 
-// start collecting socket.io metrics
-ioMetrics(io);
+// Initialize the sources
+const sources = {
+  whenplane: 0,
+  bingo: 0,
+  other: 0,
+}
 
-io.on("connection", (socket) => {
+
+// Handle socket.io connections
+io.on("connection", async (socket) => {
+
   console.log("connected");
-  stats.connections.current += 1;
-  if (stats.connections.current > stats.connections.peak)
-    stats.connections.peak = stats.connections.current;
 
+  // Increment the appropriate metrics
+  stats.connections.current.inc(1);
+  if ((await stats.connections.current.get()).values[0].value > (await stats.connections.peak.get()).values[0].value)
+    stats.connections.peak.set((await stats.connections.current.get()).values[0].value);
+
+
+  // Determine the referrer
+  const referrer = socket.request.headers.referer ?? "Unknown";
+
+  if (/.*\.{0,1}whenplane\.com/.test(referrer)) {
+    sources.whenplane++;
+    stats.whenplane.connections.inc(1);
+  } else if (/.*\.{0,1}wanshow\.bingo/.test(referrer)) {
+    sources.bingo++;
+    stats.bingo.connections.inc(1);
+  } else {
+    sources.other++;
+  }
+
+  // Handle socket.io disconnects
   socket.on("disconnect", () => {
     console.log("disconnected");
-    stats.connections.current -= 1;
+
+    // Decrement the appropriate metrics
+    if (/.*\.{0,1}whenplane\.com/.test(referrer)) {
+      sources.whenplane--;
+      stats.whenplane.connections.dec(1);
+    } else if (/.*\.{0,1}wanshow\.bingo/.test(referrer)) {
+      sources.bingo--;
+      stats.bingo.connections.dec(1);
+    } else {
+      sources.other--;
+    }
+
+    if (rooms.youtube.includes(socket.id)) {
+      rooms.youtube = rooms.youtube.filter((id) => id !== socket.id);
+      stats.youtube.connections.dec(1);
+    }
+    if (rooms.bingo.includes(socket.id)) rooms.bingo = rooms.bingo.filter((id) => id !== socket.id);
+    if (rooms.live.includes(socket.id)) rooms.live = rooms.live.filter((id) => id !== socket.id);
+
+    stats.connections.current.dec(1);
   });
 
+
+  // Handle socket.io messages
   socket.on("message", (data, ack) => {
+    stats.thud.messages.inc(1);
+    stats.thud.messagesInbound.inc(1);
     const body = JSON.parse(data) as CoreMessage<unknown>;
 
+    // Handle any subscription messages
     if (body.type === ActionTypes.SUBSCRIBE) {
       void socket.join(body.payload as string);
+      switch (body.payload as string) {
+        case "youtube":
+          rooms.youtube.push(socket.id);
+          stats.youtube.connections.inc(1);
+          break;
+        case "bingo":
+          rooms.bingo.push(socket.id);
+          break;
+        case "live":
+          rooms.live.push(socket.id);
+          break;
+
+        default:
+          break;
+      }
       if (ack !== undefined) ack("ok");
+
+      // Handle any broadcast messages
     } else if (body.type === ActionTypes.BROADCAST) {
       const request = body as BroadcastMessage;
       console.log("broadcasting to " + request.payload.room);
+
+      switch (request.payload.room) {
+        case "youtube":
+          stats.youtube.messages.inc(1);
+          stats.youtube.messagesOutbound.inc(rooms.youtube.length);
+          break;
+
+        case "bingo":
+          stats.bingo.messages.inc(1);
+          stats.bingo.messagesOutbound.inc(rooms.bingo.length);
+          break;
+
+        case "live":
+          stats.live.messages.inc(1);
+          stats.live.messagesOutbound.inc(rooms.live.length);
+          break;
+      }
+
       void socket
         .to(request.payload.room)
         .emit(
@@ -81,40 +158,64 @@ io.on("connection", (socket) => {
           JSON.stringify(request.payload.data),
         );
       if (ack !== undefined) ack("ok");
+
+      // Handle any leave messages
     } else if (body.type === ActionTypes.LEAVE) {
       void socket.leave(body.payload as string);
-      if (ack !== undefined) ack("ok");
+      switch (body.payload as string) {
+        case "youtube":
+          rooms.youtube = rooms.youtube.filter((id) => id !== socket.id);
+          stats.youtube.connections.dec(1);
+          break;
+        case "bingo":
+          rooms.bingo = rooms.bingo.filter((id) => id !== socket.id);
+          break;
+        case "live":
+          rooms.live = rooms.live.filter((id) => id !== socket.id);
+          break;
+
+        default:
+          break;
+      }
     } else {
       console.log(body);
     }
   });
 });
 
+// Handle the root route
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.get("/stats", (req, res) => {
-  res.json(stats);
-});
-
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-app.get("/metrics", async(req, res) => {
-  res.setHeader("Content-Type", promRegister.contentType);
-  res.send(await promRegister.metrics());
+app.get("/metrics", async (req, res) => {
+  res.setHeader("Content-Type", prometheus.contentType);
+  res.send(await prometheus.register.metrics());
 });
 
-app.post("/yt/:id", async (req, res) => {
-    io.to("youtube").emit("stream_detector", JSON.stringify({
-        id: req.params.id,
-        body: req.body
-    }));
-    res.status(200).send("ok");
+app.post("/yt/:id", (req, res) => {
+  stats.thud.messages.inc(1);
+  stats.youtube.messages.inc(1);
+  stats.youtube.messagesInbound.inc(1);
+  stats.youtube.messagesOutbound.inc(rooms.youtube.length);
+
+  io.to("youtube").emit("stream_detector", JSON.stringify({
+    id: req.params.id,
+    body: req.body
+  }));
+  res.status(200).send("ok");
 })
 
+// Handle bingo tile updates
 app.post("/tiles", (req, res) => {
   const data = req.body;
-  console.log(data);
+
+  stats.thud.messages.inc(1);
+  stats.bingo.messages.inc(1);
+  stats.bingo.messagesInbound.inc(1);
+  stats.bingo.messagesOutbound.inc(rooms.bingo.length);
+
   io.to(data.payload.room).emit(
     data.payload.event ?? "message",
     JSON.stringify(data.payload.data),
